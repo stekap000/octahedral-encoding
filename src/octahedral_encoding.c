6b8b141ef436fa4bba69dda5ccf4d34e501bffb5
stekap000 <stekap000@gmail.com>
2024-07-21 22:40:15 +0200

    init

--- src/octahedral_encoding.c
+++ src/octahedral_encoding.c
@@ -1,16 +1,7 @@
-#define MPC_DEBUG
-#define MPC_IMPLEMENTATION
-#include "mpc.h"
 #include <stdio.h>
 
 int main() {
-	Program program = mpc_load_program("meta.c"); (void)program;
-	//printf("%d\n", program.size);
-	//printf("%s\n", program.bytes);
-	//Token my_type = {"my_type", 8};
+	printf("Init\n");
 	
-	//mpc_token_recognize(program, my_type);
-	//int n = mpc_write_program("copy.h", program);
-	//printf("%d\n", n);
 	return 0;
 }
