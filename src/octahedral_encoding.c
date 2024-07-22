6bdfb68bfd027dd82c83408a5304359f6586c60c
stekap000 <stekap000@gmail.com>
2024-07-22 21:19:33 +0200

    some prep

--- src/octahedral_encoding.c
+++ src/octahedral_encoding.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+typedef struct {
+	float x, y, z;
+} v3;
+
+typedef struct {
+	float x, y;
+} v2;
+
+v2 octahedral_encode(v3 v) {
+	v2 w = {0};
+
+	(void)v;
+
+	// TODO: Map unit vector to octahedron.
+	// TODO: Map resulting vector to 2D space.
+	// TODO: Map 2D space to UV space.
+
+	return w;
+}
+
 int main() {
 	printf("Init\n");
 	
