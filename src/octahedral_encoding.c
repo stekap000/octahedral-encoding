1084612fb95ded0a6af46e5401170c3ca6d02424
stekap000 <stekap000@gmail.com>
2024-07-23 22:47:26 +0200

    helper vector functions

--- src/octahedral_encoding.c
+++ src/octahedral_encoding.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 typedef struct {
 	float x, y, z;
@@ -7,6 +8,19 @@ typedef struct {
 typedef struct {
 	float x, y;
 } v2;
+
+float v3_norm(v3 v) {
+	return sqrt(v.x*v.x + v.y*v.y + v.z*v.z);
+}
+
+v3 v3_unit(v3 v) {
+	float norm = v3_norm(v);
+	return (v3){v.x/norm, v.y/norm, v.z/norm};
+}
+
+float v3_dot(v3 v, v3 w) {
+	return v.x*w.x + v.y*v.y + v.z*w.z;
+}
 
 v2 octahedral_encode(v3 v) {
 	v2 w = {0};
